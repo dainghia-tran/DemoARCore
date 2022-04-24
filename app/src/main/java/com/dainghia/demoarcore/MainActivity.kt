237af72ca0d36205dd39326167e028c2d8bbd232
Dai Nghia Tran <nghia.tran@orientsoftware.com>
2022-04-24 11:05:16 +0700

    Load model to plane

--- app/src/main/java/com/dainghia/demoarcore/MainActivity.kt
+++ app/src/main/java/com/dainghia/demoarcore/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.dainghia.demoarcore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.ux.ArFragment
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arFragment = (supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment)
+
+        arFragment.setOnTapPlaneGlbModel("models/pikachu.glb")
     }
 }
